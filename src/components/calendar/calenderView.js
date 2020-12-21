a977c4fd6912e353edd0f5814c38d9e4e6d19bf7
Harsh Gupta <harshgupta.harry@gmail.com>
2020-12-21 23:48:35 +0530

    abc

--- src/components/calendar/calenderView.js
+++ src/components/calendar/calenderView.js
@@ -2,7 +2,7 @@ import React from "react";
 import InfiniteCalendar from "react-infinite-calendar";
 import "react-infinite-calendar/styles.css"; // Make sure to import the default stylesheet
 import format from "date-fns/format";
-import "./CalenderView.css";
+import "../styles/CalenderView.css";
 
 function CalenderView() {
   // Render the Calendar
@@ -14,32 +14,40 @@ function CalenderView() {
   );
   const [date, setDate] = React.useState("");
   const handleDateChange = (date) => {
-    setDate(date);
+    setDate(format(date, "yyyy-MM-dd"));
+    console.log(format(date, "yyyy-MM-dd"));
   };
 
   return (
-    <div>
-      <div>
-        <input type="text" value="{date}" onChange={handleDateChange} />
-        <button className="example-custom-input" onClick={handleDateChange}>
-          {date} Click me
-        </button>
+    <div className="row mt-5">
+      <div class="form-group">
+        <div class="input-group date" id="timepicker">
+          <span class="input-group-addon">
+            <span class="glyphicon glyphicon-calendar"></span>{" "}
+          </span>
+          <input
+            type="text"
+            value={date}
+            className="datepicker form-control"
+            placeholder="Pick date"
+            onChange={handleDateChange}
+          />
+        </div>
       </div>
+      <div></div>
       <InfiniteCalendar
         selected={null}
-        width={Math.min(window.innerWidth, 300)}
-        height={400}
+        width={Math.min(window.innerWidth, 362)}
+        height={window.innerHeight - 200}
         rowHeight={50}
-        disabledDays={[0, 6]}
         minDate={lastWeek}
         displayOptions={{
           showOverlay: false,
           shouldHeaderAnimate: false,
           showHeader: false,
+          showTodayHelper: false,
         }}
-        onSelect={function (date) {
-          alert("You selected: " + format(date, "yyyy-MM-dd"));
-        }}
+        onSelect={handleDateChange}
         theme={{
           weekdayColor: "#142A51",
           headerColor: "#142A51",
